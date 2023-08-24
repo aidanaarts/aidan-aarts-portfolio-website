882656d5faef08b72b730d634595b0dad426fe15
aidan <aidan.aarts@redvanworkshop.com>
2023-08-24 12:26:08 -0600

    Finishing Touches
    Made Responsive and filled out all details

--- assets/js/script.js
+++ assets/js/script.js
@@ -371,4 +371,23 @@ window.onload = function() {
 
 
 /*Animate*/
+function sendMail() {
+	var params = {
+		name: document.getElementById("name").value,
+		email: document.getElementById("email").value,
+		message: document.getElementById("message").value,
+	};
+	const serviceID = "service_jeyr0vu";
+	const templateID ="template_vwmm9cb";
+
+	emailjs.send(serviceID,templateID, params)
+	.then((res) => {
+		document.getElementById("name").value = "";
+		document.getElementById("email").value = "";
+		document.getElementById("message").value = "";
+		console.log(res);
+		alert("Your message sent successfully");
+	})
+	.catch((err) => console.log(err));
+}
 
